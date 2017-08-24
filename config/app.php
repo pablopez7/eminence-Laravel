8d6fe0c2e658fd7e2a7f753f703c2fce374a6cf0
pablopez7 <pablopez7@gmail.com>
2017-08-24 09:00:52 -0500

    Actualizaciones
    Actualizaciones

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Cornford\Googlmapper\MapperServiceProvider::class,
 
     ],
 
@@ -229,6 +230,7 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Mapper' => Cornford\Googlmapper\Facades\MapperFacade::class,
 
     ],
 
